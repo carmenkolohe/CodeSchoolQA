91f5866405698389a9766e235745b4df2801bd91
Suprabhat Kumar <47072868+suprabhat25@users.noreply.github.com>
2021-10-19 15:41:17 +0530

    Destructuring within the map
    Co-authored-by: Jen <52062192+jendevelops@users.noreply.github.com>

--- components/questions/QuestionList.js
+++ components/questions/QuestionList.js
@@ -5,7 +5,7 @@ function QuestionList(props) {
   const { questions } = props;
   return (
     <ul className={styles.list}>
-      {questions.map((question) => (
+      {questions.map(({id, questionBody, date}) => (
         <Question
           key={question.id}
           questionBody={question.questionBody}
